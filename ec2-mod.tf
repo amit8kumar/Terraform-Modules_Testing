f7f4d5ff1b3800e4af3bfc04337ecf7acdff6036
Amit Kumar <58173216+amit8kumar@users.noreply.github.com>
2022-08-03 18:51:51 +0530

    Update ec2-mod.tf

--- ec2-mod.tf
+++ ec2-mod.tf
@@ -12,52 +12,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "elb_http" {
-  source  = "terraform-aws-modules/elb/aws"
-  version = "~> 2.0"
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
 
-  name = "elb-example"
+  bucket = "my-s3-bucket"
+  acl    = "private"
 
-  subnets         = ["subnet-12345678", "subnet-87654321"]
-  security_groups = ["sg-12345678"]
-  internal        = false
-
-  listener = [
-    {
-      instance_port     = 80
-      instance_protocol = "HTTP"
-      lb_port           = 80
-      lb_protocol       = "HTTP"
-    },
-    {
-      instance_port     = 8080
-      instance_protocol = "http"
-      lb_port           = 8080
-      lb_protocol       = "http"
-      ssl_certificate_id = "arn:aws:acm:eu-west-1:235367859451:certificate/6c270328-2cd5-4b2d-8dfd-ae8d0004ad31"
-    },
-  ]
-
-  health_check = {
-    target              = "HTTP:80/"
-    interval            = 30
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 5
+  versioning = {
+    enabled = true
   }
 
-  access_logs = {
-    bucket = "my-access-logs-bucket"
-  }
-
-  // ELB attachments
-  number_of_instances = 2
-  instances           = ["i-06ff41a77dfb5349d", "i-4906ff41a77dfb53d"]
-
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
 }
   
 variable "AWS_ACCESS_KEY_ID" {
