20fd5b56409fb0c96b35d5f02eafc4896279adf4
Amit Kumar <58173216+amit8kumar@users.noreply.github.com>
2022-08-03 18:21:33 +0530

    Create ec2-mod.tf

---
+++ ec2-mod.tf
@@ -0,0 +1,32 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.24.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = false
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "test"
+  }
+}
