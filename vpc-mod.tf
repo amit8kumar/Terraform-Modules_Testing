f0bb212db32bf0456575f2346f0bd44e2812cc78
Amit Kumar <58173216+amit8kumar@users.noreply.github.com>
2022-08-03 21:24:46 +0530

    Update vpc-mod.tf

--- vpc-mod.tf
+++ vpc-mod.tf
@@ -9,28 +9,14 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "my-vpc"
-  cidr = "10.0.0.0/17"
-
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-
-  tags = {
-    Terraform = "yes"
-    Environment = "dev"
-    purpose     = "modtest"
-  }
+resource "aws_instance" "test-vm" {
+  instance_type = "t2.micro"
+  ami = "ami-0ff8a91507f77f867"
 }
+
   
 variable "AWS_ACCESS_KEY_ID" {
   default = "AWS_ACCESS_KEY_ID"
