b45c57fb59f2cf478d108fc93f9db24978821827
Amit Kumar <58173216+amit8kumar@users.noreply.github.com>
2022-08-03 18:41:40 +0530

    Update ec2-mod.tf

--- ec2-mod.tf
+++ ec2-mod.tf
@@ -12,23 +12,50 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
+module "elb_http" {
+  source  = "terraform-aws-modules/elb/aws"
+  version = "~> 2.0"
 
-  for_each = toset(["one", "two", "three"])
+  name = "elb-example"
 
-  name = "instance-${each.key}"
+  subnets         = ["subnet-12345678", "subnet-87654321"]
+  security_groups = ["sg-12345678"]
+  internal        = false
 
-  ami                    = "ami-ebd02392"
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
+  listener = [
+    {
+      instance_port     = 80
+      instance_protocol = "HTTP"
+      lb_port           = 80
+      lb_protocol       = "HTTP"
+    },
+    {
+      instance_port     = 8080
+      instance_protocol = "http"
+      lb_port           = 8080
+      lb_protocol       = "http"
+      ssl_certificate_id = "arn:aws:acm:eu-west-1:235367859451:certificate/6c270328-2cd5-4b2d-8dfd-ae8d0004ad31"
+    },
+  ]
+
+  health_check = {
+    target              = "HTTP:80/"
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+  }
+
+  access_logs = {
+    bucket = "my-access-logs-bucket"
+  }
+
+  // ELB attachments
+  number_of_instances = 2
+  instances           = ["i-06ff41a77dfb5349d", "i-4906ff41a77dfb53d"]
 
   tags = {
-    Terraform   = "true"
+    Owner       = "user"
     Environment = "dev"
   }
 }
